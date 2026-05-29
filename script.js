26ccbc64479bd383f310b9310b43538ee5dcef54
astra-we <emiv6442@gmail.com>
2026-05-29 06:30:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll", () => {
 
+  const header = document.querySelector("header");
+
+  if(window.scrollY > 50){
+
+    header.style.padding = "15px 8%";
+
+  }else{
+
+    header.style.padding = "20px 8%";
+
+  }
+
+});
