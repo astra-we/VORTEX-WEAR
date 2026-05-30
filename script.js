c25877124bba52c045f7f326083810d7be785b70
astra-we <emiv6442@gmail.com>
2026-05-30 01:27:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,41 @@ hiddenElements.forEach((el)=>{
   observer.observe(el);
 
 });
+const filterButtons =
+document.querySelectorAll(".filtro-btn");
+
+const cards =
+document.querySelectorAll(".card");
+
+filterButtons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+filterButtons.forEach(btn=>
+btn.classList.remove("active"));
+
+button.classList.add("active");
+
+const filter =
+button.dataset.filter;
+
+cards.forEach(card=>{
+
+if(
+filter==="all" ||
+card.dataset.category===filter
+){
+
+card.style.display="block";
+
+}else{
+
+card.style.display="none";
+
+}
+
+});
+
+});
+
+});
